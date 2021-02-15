fd364bb1da9b872ef05277e5618f75c29aca9e63
itumeleng <itumelengtshabalala8@gmail.com>
2021-02-15 21:25:49 +0200

    Itumeleng modified the route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { MenuPage } from './pages/menu/menu/menu.page';
+
+cimport { MenuPage } from './pages/menu/menu/menu.page';
 
 const routes: Routes = [
   {
@@ -50,10 +51,65 @@ const routes: Routes = [
       {
         path: 'event',
         loadChildren: () => import('./pages/event/event/event.module').then( m => m.EventPageModule)
-      },
+       },
+
+      //  new 
+      nPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/auth/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'tabs-pages',
+    loadChildren: () => import('./pages/user/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'properties',
+    loadChildren: () => import('./pages/user/property-management/properties/properties.module').then( m => m.PropertiesPageModule)
+  },
+  {
+    path: 'property-list',
+    loadChildren: () => import('./pages/user/property-management/components/property-list/property-list.module').then( m => m.PropertyListPageModule)
+  },
+  {
+    path: 'property-details',
+    loadChildren: () => import('./pages/user/property-management/property-details/property-details.module').then( m => m.PropertyDetailsPageModule)
+  },
+  {
+    path: 'appointment',
+    loadChildren: () => import('./pages/user/property-management/appointment/appointment.module').then( m => m.AppointmentPageModule)
+  },
+  {
+    path: 'car-washes',
+    loadChildren: () => import('./pages/user/carwash-management/car-washes/car-washes.module').then( m => m.CarWashesPageModule)
+  },
+  {
+    path: 'events',
+    loadChildren: () => import('./pages/user/event-management/events/events.module').then( m => m.EventsPageModule)
+  },
+  {
+    path: 'carwash-details',
+    loadChildren: () => import('./pages/user/carwash-management/carwash-details/carwash-details.module').then( m => m.CarwashDetailsPageModule)
+  },
+  {
+    path: 'book-slot',
+    loadChildren: () => import('./pages/user/carwash-management/book-slot/book-slot.module').then( m => m.BookSlotPageModule)
+  },
+  {
+    path: 'event-details',
+    loadChildren: () => import('./pages/user/event-management/event-details/event-details.module').then( m => m.EventDetailsPageModule)
+  },
+  
+
     ]
   
-  },  {
+  },
+  {
     path: 'model',
     loadChildren: () => import('./pages/Model/model/model.module').then( m => m.ModelPageModule)
   },
@@ -63,9 +119,7 @@ const routes: Routes = [
 
 
 
-];
-
-@NgModule({
+({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
