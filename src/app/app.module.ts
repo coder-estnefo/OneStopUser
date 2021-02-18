1a6bfa223a59b1761b09d70da5636e97632c58e5
coder-estnefo <ofentsemasita@gmail.com>
2021-02-18 10:59:05 +0200

    18/02/2021 10:59 - added angularefire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuPage } from './pages/menu/menu/menu.page';
-
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [AppComponent,MenuPage],
+  declarations: [AppComponent, MenuPage],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
