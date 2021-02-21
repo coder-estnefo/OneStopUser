fd804b071938421a0b85306091bc9baec201a0c1
itumeleng <itumelengtshabalala8@gmail.com>
2021-02-21 12:13:52 +0200

    all files

---
+++ .history/src/app/pages/user/favorites/favorites.page_20210221120316.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PropertiesService } from 'src/app/services/properties/properties.service';
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.page.html',
+  styleUrls: ['./favorites.page.scss'],
+})
+export class FavoritesPage implements OnInit {
+
+  constructor(
+    private _propertyService: PropertiesService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  getFavoriteProperties(){
+    
+  }
+}
---
+++ .history/src/app/services/properties/properties.service_20210221120133.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PropertiesService {
+
+  constructor(
+    private fireAuth: AngularFireAuth,
+    private firestore: AngularFirestore,
+  ) { }
+
+  // Get properties
+  getProperties(){
+    return this.firestore.collection('Properties').snapshotChanges();
+  }
+
+  // Get property by Id
+  getPropertyById(property_id: string){
+    return this.firestore.collection('Properties').doc(property_id).snapshotChanges();
+  }
+
+  // get favorite properties
+  getFavouriteProperties(){
+    return this.firestore.collection('Properties', ref => ref.where('favorite', '==', true)).
+  }
+
+  // Get user appointments
+  getUserAppointments(user_id){
+    
+  }
+
+  // Set property favorite
+  setFavoriteProperty(product_id: string, favorite: boolean){
+    return this.firestore.collection('Properties').doc(product_id).update({
+      favorite: favorite,
+    });
+  }
+
+  // Set appointment
+  setAppointment(property_id, appointment){
+    return this.firestore.collection('Properties').doc(property_id)
+      .collection('Appointments').add({
+        name: appointment.name,
+        phone: appointment.phone,
+        date: appointment.date,
+        time: appointment.time,
+        email: appointment.email  
+      });
+  }
+}
