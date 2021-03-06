51659c903c5c39f2b1fa8ab2c982bd79a230bcf4
tearz007 <54117577+tearz007@users.noreply.github.com>
2021-03-06 18:30:50 +0200

    edited calender

--- src/app/pages/user/property-management/property-details/property-details.page.ts
+++ src/app/pages/user/property-management/property-details/property-details.page.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ActivatedRoute, Router, ParamMap} from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { PropertiesService } from 'src/app/services/properties/properties.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { IProperty } from 'src/app/structures/interfaces';
+import firebase from 'firebase/app'
 
 @Component({
   selector: 'app-property-details',
@@ -12,8 +13,9 @@ import { IProperty } from 'src/app/structures/interfaces';
 })
 export class PropertyDetailsPage implements OnInit {
 
-  userID;
+  userID = firebase.auth().currentUser.uid;
   userDetails;
+
   property: IProperty;
 
   options = {
@@ -30,68 +32,80 @@ export class PropertyDetailsPage implements OnInit {
   ) { }
 
   ngOnInit() {
-     this.auth.authState.subscribe((user) => {
+    /*this.auth.authState.subscribe((user) => {
       this.userID = user.uid;
-      this.userService.getUser(this.userID).subscribe((user)=> {
+      this.userService.getUser(this.userID).subscribe((user) => {
         this.userDetails = user.payload.data();
+
       })
-    });
+
+
+    });*/
+
+    console.log(this.userID)
+
+    this.userService.getUser(this.userID).subscribe(data => {
+      let id, userInfor;
+      userInfor = data.payload.data();
+      this.userDetails = userInfor;
+      
+    })
 
     const property_id: string = this.activatedRoute.snapshot.paramMap.get('id');
     this.getPropertyById(property_id);
   }
 
-  gotoSetAppointment(){
+  gotoSetAppointment() {
     // this.router.navigateByUrl('appointment');
     this.router.navigateByUrl('notification');
   }
 
-  getPropertyById(property_id: string){
+  getPropertyById(property_id: string) {
     let id, property, temp_property;
     this.property = null;
     this._propertyService.getPropertyById(property_id).subscribe(
       response => {
         id = response.payload.id;
         property = response.payload.data();
-        console.log(property)
+
         temp_property = {
-            id: id,
-            name: property.name,
-            address: property.location,
-            images: property.images,
-            price: property.price,
-            garages: property.garages,
-            bedrooms: property.bedrooms,
-            bathrooms: property.bathrooms,
-            description: property.description,
-            availability_status: property.availability,
-            features: property.features,
-            favorite: property.favorite,
-            ownerID: property.ownerID
+          id: id,
+          name: property.name,
+          address: property.location,
+          images: property.images,
+          price: property.price,
+          garages: property.garages,
+          bedrooms: property.bedrooms,
+          bathrooms: property.bathrooms,
+          description: property.description,
+          availability_status: property.availability,
+          features: property.features,
+          favorite: property.favorite,
+          ownerID: property.ownerID
         }
         this.property = temp_property;
-        console.log(this.property);
+
       }
     )
   }
 
   startChat(id, ownerID, propertyName) {
-    const from = this.userID;
-    const to = ownerID;
-    const message = 'I am interested in this property: '+ propertyName;
-    const date = new Date();
-    const time = date.getHours() + ':' + date.getMinutes();
-    const senderName = this.userDetails.name;
-    const chat = { id, message, from, to, time, date , senderName};
 
-    console.log(to)
-    // this._propertyService.startChat(chat).then(()=>{
-    //   this.router.navigate(['/tabs-pages/tabs/chats/'+to]);
-    // })
-    this.router.navigate(['/messages/'+id], {
-      queryParams: { propertyID: id, userID: from, sendTo: to },
-    });
+   const from = this.userID;
+     const to = ownerID;
+     const message = 'I am interested in this property: '+ propertyName;
+     const date = new Date();
+     const time = date.getHours() + ':' + date.getMinutes();
+     // const senderName = this.userDetails.name;
+     const chat = { id, message, from, to, time, date};
+ 
+     // this._propertyService.startChat(chat).then(()=>{
+     //   this.router.navigate(['/tabs-pages/tabs/chats/'+to]);
+     // })
+     this.router.navigate(['/messages/'+id], {
+       queryParams: { propertyID: id, userID: from, sendTo: to },
+     });
   }
 
-  
+
 }
