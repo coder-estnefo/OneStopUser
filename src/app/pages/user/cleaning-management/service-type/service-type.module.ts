c16237a9fbe4d51bf7d00cb487cd94cbb82bd325
Itumeleng-Tshabalala <itumelengtshabalala8@gmail.com>
2021-03-15 15:58:53 +0200

    all'

--- src/app/pages/user/cleaning-management/service-type/service-type.module.ts
+++ src/app/pages/user/cleaning-management/service-type/service-type.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { ServiceTypePage } from './service-type.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ServiceTypePageRoutingModule
+    ServiceTypePageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ServiceTypePage]
 })
-export class ServiceTypePageModule {}
+export class ServiceTypePageModule { }
