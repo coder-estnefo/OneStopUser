507fddf1cf3186ca80033ee1d74d29442d0f2175
coder-estnefo <ofentsemasita@gmail.com>
2021-03-16 23:52:17 +0200

    update

--- src/app/services/properties/properties.service.ts
+++ src/app/services/properties/properties.service.ts
@@ -3,37 +3,40 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PropertiesService {
-
   constructor(
     private fireAuth: AngularFireAuth,
-    private firestore: AngularFirestore,
-  ) { }
+    private firestore: AngularFirestore
+  ) {}
 
   // Get properties
   getProperties() {
     return this.firestore
-      .collection('Properties', ref => ref.where('availability_status', '==', true))
+      .collection('Properties', (ref) =>
+        ref.where('availability_status', '==', true)
+      )
       .snapshotChanges();
   }
 
   // Get property by Id
   getPropertyById(property_id: string) {
-    return this.firestore.collection('Properties').doc(property_id).snapshotChanges();
+    return this.firestore
+      .collection('Properties')
+      .doc(property_id)
+      .snapshotChanges();
   }
 
   // get favorite properties
   getFavoriteProperties() {
-    return this.firestore.collection('Properties', ref => ref.where('favorite', '==', true))
+    return this.firestore
+      .collection('Properties', (ref) => ref.where('favorite', '==', true))
       .snapshotChanges();
   }
 
   // Get user appointments
-  getUserAppointments(user_id) {
-
-  }
+  getUserAppointments(user_id) {}
 
   // Set property favorite
   setFavoriteProperty(product_id: string, favorite: boolean) {
@@ -44,13 +47,16 @@ export class PropertiesService {
 
   // Set appointment
   setAppointment(property_id, appointment) {
-    return this.firestore.collection('Properties').doc(property_id)
-      .collection('Appointments').add({
+    return this.firestore
+      .collection('Properties')
+      .doc(property_id)
+      .collection('Appointments')
+      .add({
         name: appointment.name,
         phone: appointment.phone,
         date: appointment.date,
         time: appointment.time,
-        email: appointment.email
+        email: appointment.email,
       });
   }
 
@@ -66,17 +72,11 @@ export class PropertiesService {
   startChat(chat) {
     const { id, message, from, to, time, date, propertyName } = chat;
     const chatID = this.setChatID(from, to) + id;
-    return this.firestore.collection('chats').doc(from).collection('messages').add({
-      id,
-      message,
-      from,
-      to,
-      time,
-      date,
-      chatID,
-      propertyName,
-    }).then(() => {
-      return this.firestore.collection('chats').doc(to).collection('messages').add({
+    return this.firestore
+      .collection('chats')
+      .doc(from)
+      .collection('messages')
+      .add({
         id,
         message,
         from,
@@ -84,14 +84,33 @@ export class PropertiesService {
         time,
         date,
         chatID,
-        propertyName
+        propertyName,
       })
-    })
+      .then(() => {
+        return this.firestore
+          .collection('chats')
+          .doc(to)
+          .collection('messages')
+          .add({
+            id,
+            message,
+            from,
+            to,
+            time,
+            date,
+            chatID,
+            propertyName,
+          });
+      });
   }
 
   //get chats
   getChats(userID) {
-    return this.firestore.collection('chats').doc(userID).collection('messages').snapshotChanges();
+    return this.firestore
+      .collection('chats')
+      .doc(userID)
+      .collection('messages')
+      .snapshotChanges();
   }
 
   // //Chat
@@ -122,4 +141,13 @@ export class PropertiesService {
   //     return uid2 + uid1;
   //   }
   // }
+
+  getViewingDates(ownerID) {
+    return this.firestore
+      .collection('Owner')
+      .doc(ownerID)
+      .collection('Property_Dates')
+      .doc(ownerID)
+      .snapshotChanges();
+  }
 }
