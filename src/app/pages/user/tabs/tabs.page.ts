b08b3c286c3f83ccfe72952b34424c26beae93ec
tearz007 <54117577+tearz007@users.noreply.github.com>
2021-03-28 21:00:25 +0200

    update

--- src/app/pages/user/tabs/tabs.page.ts
+++ src/app/pages/user/tabs/tabs.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PropertiesService } from 'src/app/services/properties/properties.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  userID;
+  propertyChats=[]
+
+  constructor(
+    private router: Router,
+    private activatedRoute:ActivatedRoute,
+    private propertiesService: PropertiesService,
+    private auth: AngularFireAuth
+  ) { 
+
+  }
 
   ngOnInit() {
+    this.auth.authState.subscribe((user) => {
+      if (user) {
+        this.userID = user.uid;
+        this.getPropertiesChats();
+      }
+    });
   }
+
+  getPropertiesChats() {
+    this.propertiesService.getChats(this.userID).subscribe((response) => {
+      this.propertyChats = response.map((chats) => {
+        return {
+          id: chats.payload.doc.id,
+          ...(chats.payload.doc.data() as Object),
+        };
+      });
+
+      const temp_chats = this.propertyChats.sort((a, b) => b.date - a.date);
+      this.propertyChats = temp_chats;
+    })
+
+    
+  }
+
 
 }
